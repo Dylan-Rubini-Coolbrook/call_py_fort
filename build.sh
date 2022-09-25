f1be72e262ea25551a443d5f287d82b6895433c7
dylanrubini <dylan.rubini@hotmai.co.uk>
2022-09-25 21:51:13 +0100

    flags

--- build.sh
+++ build.sh
@@ -16,11 +16,14 @@ conda activate ct-build
 export FC=gfortran
 export F90=gfortan
 export F77=gfortran
+export CC=gcc
+export CXX=g++
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 sudo rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_Fortran_FLAGS="-O3"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS="-O3 -march=native -mtune=native"
 make
 make test
 
