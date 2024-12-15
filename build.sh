b2044f92776361554177cf8679251d01adc644b8
Dylan Rubini <dylan.rubini@hotmail.co.uk>
2024-12-15 13:24:10 +0000

    [Build]

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-module purge
-# set-up/source spack environment variables
-unset LD_LIBRARY_PATH
-# set-up/source spack environment variables
-source /datapart1/spack/share/spack/setup-env.sh
-spack clean
-spack unload --all
-
-# load modules from spack
-module load gcc-11.2.0-gcc-7.3.0-67ocnsa
 
 TF_VERSION="2"
 TF_ENV_NAME="tf-cpu-source${TF_VERSION}"
@@ -24,10 +14,27 @@ export CC=gcc
 export CXX=g++
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
+# Determine the OS type
+os_type=$(uname -s)
+cpu_arch=$(uname -m)
+
+pre-commit install
+
+if [ "$os_type" = "Darwin" ] && [ "$cpu_arch" = "arm64" ]; then
+    echo "This is macOS."
+    FLAGS="armv9.2-a"
+
+elif [ "$os_type" = "Linux" ]; then
+	echo "This is Linux"
+    FLAGS="-march=native -mtune=native"
+else
+	exit 1
+fi
+
 sudo rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS="-O3 -march=native -mtune=native -Wl,-rpath=/home/orie3565/micromamba/envs/${TF_ENV_NAME}/lib/"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS="-O3 ${FLAGS} -Wl,-rpath ${CONDA_PREFIX}/lib/"
 make
 make test
 
